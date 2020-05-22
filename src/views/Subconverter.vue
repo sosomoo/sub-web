5183cf31c177ee2b96d83e8fb2f68655b5b49726
sosomoo <47846341+sosomoo@users.noreply.github.com>
2020-05-22 17:51:03 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -222,8 +222,8 @@ const project = "https://github.com/CareyWang/sub-web";
 const remoteConfigSample =
   "https://raw.githubusercontent.com/tindy2013/subconverter/master/base/config/example_external_config.ini";
 const gayhubRelease = "https://github.com/tindy2013/subconverter/releases";
-const defaultBackend = "https://api.wcc.best/sub?";
-const shortUrlBackend = "https://api.suo.yt/short";
+const defaultBackend = "https://api.socloud.win/sub?";
+const shortUrlBackend = "https://api.socloud.win/short";
 const configUploadBackend = "https://api.wcc.best/config/upload";
 const tgBotLink = "https://t.me/CareyWong_bot";
 
